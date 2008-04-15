3a6c4ef5bd16c1266ab0c5b85310c3a83fccdacc
Alexandre Girard <alx.girard@gmail.com>
2008-04-15 14:11:27 +0200

    change error level

--- PHP-server-1.1/src/DB.php
+++ PHP-server-1.1/src/DB.php
@@ -959,7 +959,7 @@ class DB_Error extends PEAR_Error
      * @see PEAR_Error
      */
     function DB_Error($code = DB_ERROR, $mode = PEAR_ERROR_RETURN,
-                      $level = E_USER_NOTICE, $debuginfo = null)
+                      $level = E_USER_ERROR, $debuginfo = null)
     {
         if (is_int($code)) {
             $this->PEAR_Error('DB Error: ' . DB::errorMessage($code), $code,
